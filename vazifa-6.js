4c42ef72a223b328f9cf4bf8b54f028cf11ad5ba
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-05-31 11:01:35 +0500

    vazifa-6 done

--- vazifa-6.js
+++ vazifa-6.js
@@ -1,4 +1,19 @@
-let arr = [1,3,4,5,6,7];
-let l = arr.length;
-let num = Math.floor(l / 2);
-let res;
+let arr = [1,3,5,4,5,10];
+// let l = arr.length;
+// let num = Math.floor(l / 2);
+// let res;
+
+
+showArr = (arr) => {
+    const l = arr.length - 1;
+
+    if(l % 2 == 0){
+        return arr[Math.floor(l / 2)];
+    }
+
+    else{
+        return (arr[Math.floor(l / 2)] + arr[Math.floor(l / 2) + 1]) / 2;
+    }
+}
+
+console.log(showArr(arr));
