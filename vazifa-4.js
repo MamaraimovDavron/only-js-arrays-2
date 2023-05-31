8659f6ea41a33e8fa334719f4a446e3837945815
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-05-31 10:18:35 +0500

    Today we started

--- vazifa-4.js
+++ vazifa-4.js
@@ -1,16 +1,41 @@
-let a = [1,3,5];
-let b = [2,6,10];
-let a1 = 0;
-let b1 = 0;
+let a = [1,3,5,19];
+let b = [2,6,10,5];
+// let a1 = 0;
+// let b1 = 0;
 
-for(let i = 0; i < a.length; i++){
-    a1 += a[i];
-    b1 += b[i];
+showArray = (a,b) => {
+    if(a.reduce((total, num) => {return total + num}) > b.reduce((total, num) => {return total + num})){
+        return a;
+    }
+
+    else{
+        return b;
+    }
 }
 
-    if(a1 > b1){
-        console.log(a);
-    }
-    else{
-        console.log(b);
-    }
+console.log(showArray(a,b));
+
+
+
+// addArr = (a, b) => {
+//     let res = 0;
+//     let res1 = 0;
+//     for(let i = 0; i < a.length; i++){
+//         res += a[i];
+//     }
+
+//     for(let i = 0; i < b.length; i++){
+//         res1 += b[i];
+//     }
+
+//     if(res > res1){
+//         return a;
+//     }
+
+//     else{
+//         return b;
+//     }
+// }
+
+
+// console.log(addArr(a,b));
--- vazifa-6.js
+++ vazifa-6.js
@@ -2,22 +2,3 @@ let arr = [1,3,4,5,6,7];
 let l = arr.length;
 let num = Math.floor(l / 2);
 let res;
-
-// if(num){
-//     res = arr[num];
-// }
-
-// if(l % 2 === 0){
-//     res = (arr[num - 1] + arr[num + 1]) / 2;
-// }
-
-// console.log(res);
-
-
-for(let i = 0; i <= l; i++){
-    if(i % 2 == 0){
-        res = (arr[i + 1] + arr[i - 1]) / 2;
-    }
-}
-
-console.log(res);
